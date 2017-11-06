12efe7389c627c47ba7db4ec1a528a8e60d9456b
OscarZhou <zhouhongyu1989@gmail.com>
2017-11-06 23:36:12 +1300

    Successfully configure the router for all static files

--- routers/router.go
+++ routers/router.go
@@ -1,57 +1,57 @@
 package routers
 
-import(
-	"github.com/gin-gonic/gin"
-	"oscar-health-go/controllers"
+import (
 	"fmt"
-	"github.com/gin-contrib/sessions"
+	"net/http"
+	"oscar-health-go/controllers"
 	"oscar-health-go/utils"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
 )
 
-var(
-	router		*gin.Engine
-	store = sessions.NewCookieStore([]byte("secret"))
+var (
+	router *gin.Engine
+	store  = sessions.NewCookieStore([]byte("secret"))
 )
 
-func init(){
+func init() {
 	fmt.Println("..................Init Router")
 	router = gin.Default()
 	configRouter()
 }
 
-func NewRouter() *gin.Engine{
+func NewRouter() *gin.Engine {
 	return router
 }
 
 func configRouter() {
 	fmt.Println("...................Configure Router")
 
-	router.Static("/static/", "/css")
+	//配置静态路由，表示请求的uri为/static时，显示的是/static文件夹下的内容
+	router.StaticFS("/static", http.Dir("static"))
 
 	router.GET("/", (&controllers.DefaultController{}).Index)
 
 	router.Use(sessions.Sessions("gosession", store))
-	{
-		router.GET("login", (&controllers.OauthController{}).LoginByAuth)
-		// router.GET("auth", (&controllers.OauthController{}).AuthorizationCodeMethod)
-		router.GET("auth", (&controllers.OauthController{}).TestMethod)
-	}
+
+	router.GET("login", (&controllers.OauthController{}).LoginByAuth)
+	// router.GET("auth", (&controllers.OauthController{}).AuthorizationCodeMethod)
+	router.GET("auth", (&controllers.OauthController{}).TestMethod1)
 
 	v1 := router.Group("/webapi/v1", utils.AuthorizeAPIToken)
-	{
-		v1.GET("/webapi/v1/product/", (&controllers.ProductController{}).GetProducts)
-		
-		v1.GET("/category/", (&controllers.CategoryController{}).GetProductCategories)
-		v1.GET("/category/:id", (&controllers.CategoryController{}).GetProductCategory)
-		v1.POST("/category/", (&controllers.CategoryController{}).CreateProductCategory)
-		v1.PUT("/category/:id", (&controllers.CategoryController{}).UpdateProductCategory)
-		v1.DELETE("/category/:id", (&controllers.CategoryController{}).DeleteProductCategory)		
-		
-		v1.GET("/brand/", (&controllers.BrandController{}).GetBrands)
-		v1.GET("/brand/:id", (&controllers.BrandController{}).GetBrand)
-		v1.POST("/brand/", (&controllers.BrandController{}).CreateBrand)
-		v1.PUT("/brand/:id", (&controllers.BrandController{}).UpdateBrand)
-		v1.DELETE("/brand/:id", (&controllers.BrandController{}).DeleteBrand)		
-	}
+	v1.GET("/webapi/v1/product/", (&controllers.ProductController{}).GetProducts)
+
+	v1.GET("/category/", (&controllers.CategoryController{}).GetProductCategories)
+	v1.GET("/category/:id", (&controllers.CategoryController{}).GetProductCategory)
+	v1.POST("/category/", (&controllers.CategoryController{}).CreateProductCategory)
+	v1.PUT("/category/:id", (&controllers.CategoryController{}).UpdateProductCategory)
+	v1.DELETE("/category/:id", (&controllers.CategoryController{}).DeleteProductCategory)
+
+	v1.GET("/brand/", (&controllers.BrandController{}).GetBrands)
+	v1.GET("/brand/:id", (&controllers.BrandController{}).GetBrand)
+	v1.POST("/brand/", (&controllers.BrandController{}).CreateBrand)
+	v1.PUT("/brand/:id", (&controllers.BrandController{}).UpdateBrand)
+	v1.DELETE("/brand/:id", (&controllers.BrandController{}).DeleteBrand)
 
 }
